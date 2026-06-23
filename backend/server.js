874c646aa9f79ee246ca2b5e4d544f2abb7c71d1
alfarismenga-maker <alfarismenga@gmail.com>
2026-06-23 22:24:04 +0700

    fix cors railway

--- backend/server.js
+++ backend/server.js
@@ -3,39 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const db = require("./config/db");
 const authRoutes = require("./routes/auth");
 const orderRoutes = require("./routes/orders");
 const qrRoutes = require("./routes/qrcode");
 
 const app = express();
 
-/*
-  CORS FIX
-*/
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.options("*", cors());
 
 app.use(express.json());
 
@@ -47,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("LaundryGo API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
